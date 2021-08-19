af0f8979302b41cca1cb2d09296fe2bb3fff4ad1
Damian <damianmdst@gmail.com>
2021-08-19 10:11:51 -0300

    Setup dev tools to improve dev experience into the project

--- src/api.js
+++ src/api.js
@@ -3,39 +3,39 @@ import faker from 'faker';
 
 export default class MessageGenerator {
   constructor(options) {
-    this.messageCallback = options.messageCallback
-    this.stopGeneration = false
+    this.messageCallback = options.messageCallback;
+    this.stopGeneration = false;
   }
 
   stop() {
-    this.stopGeneration = true
+    this.stopGeneration = true;
   }
 
   start() {
-    this.stopGeneration = false
-    this.generate()
+    this.stopGeneration = false;
+    this.generate();
   }
 
   isStarted() {
-    return !this.stopGeneration
+    return !this.stopGeneration;
   }
 
   /**
-     * priority from 1 to 3, 1 = error, 2 = warning, 3 = info
-     * */
+   * priority from 1 to 3, 1 = error, 2 = warning, 3 = info
+   * */
   generate() {
     if (this.stopGeneration) {
-      return
+      return;
     }
     const message = faker.lorem.sentence();
-    const priority = random(1, 3)
-    const nextInMS = random(500, 3000)
+    const priority = random(1, 3);
+    const nextInMS = random(500, 3000);
     this.messageCallback({
       message,
       priority,
-    })
+    });
     setTimeout(() => {
-      this.generate()
-    }, nextInMS)
+      this.generate();
+    }, nextInMS);
   }
 }
